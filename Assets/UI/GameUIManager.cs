e25be0c4709dfe62e9b799bd26cbb3a76cec78eb
Joel Greensite <joel.greensite@gmail.com>
2023-02-12 07:50:48 +0000

    Starting the show/hide checkbox for the UI

--- Assets/UI/GameUIManager.cs
+++ Assets/UI/GameUIManager.cs
@@ -1,6 +1,11 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+//using UnityEngine.UI;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
+using Toggle = UnityEngine.UIElements.Toggle;
 //using Unity.Burst;
 //using Unity.Entities;
 //using Unity.Mathematics;
@@ -9,9 +14,11 @@ using UnityEngine.UIElements;
 
 public class GameUIManager : MonoBehaviour
 {
+    public Toggle m_ShowUI;
     public Button m_LeaveArea;
     public Button m_Spawn;
     public Button m_Load;
+    public Button m_Save;
     public Button m_Refresh;
     public Button m_Clear;
 
@@ -19,6 +26,8 @@ public class GameUIManager : MonoBehaviour
     public VisualElement m_HostScreen;
     public VisualElement m_JoinScreen;
     public VisualElement m_ManualConnectScreen;
+
+    private VisualElement[] elementsToHide;
 
     //public new class UxmlFactory : UxmlFactory<GameUIManager, UxmlTraits> { }
 
@@ -31,21 +40,42 @@ public class GameUIManager : MonoBehaviour
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
+        m_ShowUI = root.Q<Toggle>("show-ui");
         m_LeaveArea = root.Q<Button>("quit-game");
         m_Spawn = root.Q<Button>("spawn-board");
         m_Load = root.Q<Button>("load-board");
+        m_Save = root.Q<Button>("save-board");
         m_Refresh = root.Q<Button>("refresh-board");
         m_Clear = root.Q<Button>("clear-board");
 
+        m_ShowUI.RegisterValueChangedCallback(OnToggleValueChanged);
         m_LeaveArea.clicked += () => ClickedButton("quit-game");
         m_Spawn.clicked += () => ClickedButton("spawn-board");
         m_Load.clicked += () => ClickedButton("load-board");
+        m_Save.clicked += () => ClickedButton("save-board");
         m_Refresh.clicked += () => ClickedButton("refresh-board");
-        m_Clear.clicked += () => ClickedButton("clear-board"); 
+        m_Clear.clicked += () => ClickedButton("clear-board");
+
+        //populate the array of elements to hide with all the elements in the UI except m_ShowUI
+        elementsToHide = root.Children()
+            .Where(element => element.name != "show-ui")
+        .ToArray();
+
+        //make the toggle m_ShowUI on by default
+        m_ShowUI.value = true;
     }
 
 
-    // capture click event and navige to the correct screen
+    //capture toggle change event and show/hide the UI
+    void OnToggleValueChanged(ChangeEvent<bool> evt)
+    {
+        foreach (var element in elementsToHide)
+        {
+            element.style.display = evt.newValue ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+    }
+
+    // capture button click event and naviate to the correct screen/ call the correct function
     void ClickedButton(string txt)
     {
         var HexSpawner = GameObject.Find("HexSpawner").GetComponent<HexSpawner>();
@@ -53,6 +83,9 @@ public class GameUIManager : MonoBehaviour
         Debug.Log("Clicked" + txt);
         switch (txt)
         {
+            case "show-ui":
+                Debug.Log("Show UI");
+                break;
             case "quit-game":
                 SceneManager.LoadScene("MainMenu");
                 break;
@@ -63,6 +96,10 @@ public class GameUIManager : MonoBehaviour
             case "load-board":
                 Debug.Log("Load Board");
                 HexSpawner.LoadState(null);
+                break;
+            case "save-board":
+                Debug.Log("Save Board");
+                HexSpawner.SaveHexes(null);
                 break;
             case "refresh-board":
                 Debug.Log("Refresh Board");
