564d74294814e3c1380205e253fc7c68c7db800e
Joel Greensite <joel.greensite@gmail.com>
2023-12-29 21:02:19 +0000

    Minor update to saved state along with a change to what we are saving

--- Assets/Scripts/GameSpawner.cs
+++ Assets/Scripts/GameSpawner.cs
@@ -96,14 +96,14 @@ public class GameSpawner : SpawnerBase
 
         //write a single save state comprised of the hexSpawner state and the edgeSpawner state and the cornerSpawner state
 
-        //byte[] bytes0 = SerializationUtility.SerializeValue(spawnerStates, DataFormat.JSON);
-        //    File.WriteAllBytes(filePath + "0 .json", bytes0);
+        byte[] bytes0 = SerializationUtility.SerializeValue(spawnerStates, DataFormat.JSON);
+            File.WriteAllBytes(filePath + "0 .json", bytes0);
         byte[] bytes1 = SerializationUtility.SerializeValue(hexSpawner.State, DataFormat.JSON);
             File.WriteAllBytes(filePath + "1 .json", bytes1);
-        //byte[] bytes2 = SerializationUtility.SerializeValue(edgeSpawner.State, DataFormat.JSON);
-        //    File.WriteAllBytes(filePath + "2 .json", bytes2);
-        //byte[] bytes3 = SerializationUtility.SerializeValue(cornerSpawner.State, DataFormat.JSON);
-        //    File.WriteAllBytes(filePath + "3 .json", bytes3);
+        byte[] bytes2 = SerializationUtility.SerializeValue(edgeSpawner.State, DataFormat.JSON);
+            File.WriteAllBytes(filePath + "2 .json", bytes2);
+        byte[] bytes3 = SerializationUtility.SerializeValue(cornerSpawner.State, DataFormat.JSON);
+            File.WriteAllBytes(filePath + "3 .json", bytes3);
 
 
     }
@@ -121,6 +121,7 @@ public class GameSpawner : SpawnerBase
         {
             filePath = "./data/maps/map.json"; //default value
         }
+
         if (!File.Exists(filePath)) return; // No state to load
 
         byte[] bytes = File.ReadAllBytes(filePath);
